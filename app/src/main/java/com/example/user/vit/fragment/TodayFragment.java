2078d545bab215fcaacf122b02c6ded2421d9475
rutanshuj <rjhaveri41@gmail.com>
2017-07-28 22:32:49 +0530

    Today Fragment

--- app/src/main/java/com/example/user/vit/fragment/TodayFragment.java
+++ app/src/main/java/com/example/user/vit/fragment/TodayFragment.java
@@ -3,6 +3,8 @@ package com.example.user.vit.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +13,18 @@ import com.example.user.vit.R;
 
 public class TodayFragment extends android.support.v4.app.Fragment {
 
+    private RecyclerView recyclerView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.today_frag, container, false);
+
+        recyclerView = (RecyclerView) v.findViewById(R.id.today_sub_recyclerView);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
         return v;
     }
 }
