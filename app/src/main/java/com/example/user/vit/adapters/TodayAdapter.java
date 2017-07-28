9f34b0cdbb1ed006a221ce489f6f36df8b9a4ae4
rutanshuj <rjhaveri41@gmail.com>
2017-07-28 23:06:23 +0530

    Today Fragment Recycler View added

--- app/src/main/java/com/example/user/vit/adapters/TodayAdapter.java
+++ app/src/main/java/com/example/user/vit/adapters/TodayAdapter.java
@@ -5,14 +5,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.example.user.vit.R;
 
-import java.util.List;
-
-
 public class TodayAdapter extends RecyclerView.Adapter<TodayAdapter.MyViewHolder> {
-
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
