6d18cd853ec23bc7458ad0d4c5b89b107083c82d
rutanshuj <rjhaveri41@gmail.com>
2017-08-03 16:18:11 +0530

    Touch Image View Changed

--- app/src/main/java/com/example/user/vit/activities/MapActivity.java
+++ app/src/main/java/com/example/user/vit/activities/MapActivity.java
@@ -3,7 +3,7 @@ package com.example.user.vit.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
+
 
 import com.example.user.vit.R;
 
@@ -12,9 +12,6 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_frag);
-
-
         TouchImageView iv = (TouchImageView) findViewById(R.id.map_img);
-
     }
 }
